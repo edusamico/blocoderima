5593aca6db6b5d690b826e8b39b9303c25f88290
edusamico <edusamico@gmail.com>
2022-05-13 13:16:56 -0300

    counting vowels

--- my-app/src/components/Rimas/Rimas.jsx
+++ my-app/src/components/Rimas/Rimas.jsx
@@ -1,5 +1,7 @@
 
 const Rimas = ({ suffixRimas }) => {
+  
+// taking only unique words of object
   let rimas = [];
   for (let i in suffixRimas) {
     let rima = suffixRimas[i].word;
@@ -8,20 +10,46 @@ const Rimas = ({ suffixRimas }) => {
 
   let filterRimas = [...new Set(rimas)];
 
-  //let filterToSyllables = (word) => {}
+// counting vowels 
+  const filterToSyllables = (word) => {
+    let countVowels = 0;
+    const wordArr = word.split("");
+    const vowels = ["a", "e", "i", "o", "u",
+      "á", "é", "í", "ó", "ú",
+      "â", "ê", "î", "ô", "û",
+      "à", "è", "ì", "ò", "ù",
+      "ã", "õ"]
 
-  let count = false;
-  count = (filterRimas.length !== 0) ? true : false;
-  
+    for (let letter of wordArr) {
+      for(let vowel of vowels) {
+        if (letter === vowel) {
+          countVowels++;
+        }
+      } 
+    }   
+    return countVowels; 
+  }
+// creating lists by nSyll
+  for (let word of filterRimas) {
+    let nSyll = filterToSyllables(word);
+    console.log(`a palavra ${word} tem ${nSyll} sílabas`)
+    let arrWordSyll = [];
+  }
+
+
+//preparing to show
+  let toShow = false;
+  toShow = (filterRimas.length !== 0) ? true : false;
+
   return (
     <div>
       <h2>Rimas</h2>
-      {count && <p>Encontramos {filterRimas.length} possíveis rimas!</p>}
-      {!count && <p>Tente com outra palavra!</p>}
-     
+      {toShow && <p>Encontramos {filterRimas.length} possíveis rimas!</p>}
+      {!toShow && <p>Tente com outra palavra!</p>}
 
       <div>
         <ol className='rimas'>
+          <h3>Número de sílabas</h3>
           {filterRimas.map(filterRimas => (
             <li className='rima' key={filterRimas}>{filterRimas}</li>
           ))}
